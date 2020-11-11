480c3998a0eb77e1c6c676fe943e35b69bb7c7da
504steven <504steven@gmail.com>
2020-11-10 22:02:26 -0800

    ignore Spring default mongoDB config

--- src/main/java/com/wgc/spring_rest_service/SpringRESTWebService_CollegeRecommender/SpringRestServiceCollegeRecommenderApp.java
+++ src/main/java/com/wgc/spring_rest_service/SpringRESTWebService_CollegeRecommender/SpringRestServiceCollegeRecommenderApp.java
@@ -2,12 +2,13 @@ package com.wgc.spring_rest_service.SpringRESTWebService_CollegeRecommender;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class SpringRestServiceCollegeRecommenderApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRestServiceCollegeRecommenderApp.class, args);
 	}
-
 }
